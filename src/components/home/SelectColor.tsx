e4c7eb381427732a0b01d5d261df729fe94e68fe
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-25 20:15:55 -0500

    Add: react-color and select color image

--- src/components/home/SelectColor.tsx
+++ src/components/home/SelectColor.tsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
-import { calculateShades } from "@helpers/helpers";
-import { CardColor } from "@components/cards/CardColor";
+import { calculateShades     } from "@helpers/helpers";
+import { CardColor           } from "@components/cards/CardColor";
+import { ChromePicker, ColorResult } from "react-color";
 import "@styles/input-select.css";
 
 const SelectColor = () => {
   const [selectedColor, setSelectedColor] = useState("#0057FF");
   const [colorShades, setColorShades] = useState<string[]>([]);
 
-  const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newColor = event.target.value;
-    setSelectedColor(newColor);
+  const handleColorChange = (color: ColorResult) => {
+    setSelectedColor(color.hex);
   };
 
   useEffect(() => {
@@ -22,11 +22,11 @@ const SelectColor = () => {
       <h2 className="text-center uppercase text-3xl font-black">
         Vamos a generar tu paleta de colores!
       </h2>
-      <div className="flex items-center justify-center gap-5">
-        <span className="text-blue-400 font-medium text-xl">Selecciona tu color</span>
-        <div className="bg-primary-100/30 dark:bg-gray-100 p-4 pb-3 rounded-lg">
-          <input type="color" value={selectedColor} onChange={handleColorChange} />
-        </div>
+      <div className="flex flex-col items-center gap-5">
+        <span className="uppercase font-black text-2xl" style={{color: selectedColor}}>
+          Selecciona tu color
+        </span>
+        <ChromePicker color={selectedColor} onChange={handleColorChange} />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-x-5 gap-y-7 bg-transparent py-5">
         {colorShades.map((shade, index) => (
