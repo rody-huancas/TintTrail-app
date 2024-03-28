a7db9d62db50d0fce75d55bcad2359da19605d40
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-27 19:57:10 -0500

    Update: information footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,20 @@
+import { FaCodepen, FaGithub } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div className="max-w-5xl font-medium text-lg w-full mx-auto py-10 text-center text-secondary-100 dark:text-gray-100/90">
-      <a href="https://github.com/rody-huancas" target="_blank" aria-label="Ver GitHub de Rody Huancas">
+    <div className="max-w-6xl font-medium text-lg w-full mx-auto p-10 lg:p-0 lg:py-10 flex items-center justify-between text-secondary-100 dark:text-gray-100/90">
+      <a href="https://rodyhuancas.netlify.app/" target="_blank" aria-label="Ver portafolio de Rody Huancas">
         Rody Huancas
       </a>
+
+      <div className="flex items-center gap-4">
+        <a href="https://github.com/rody-huancas" target="_blank" className="rounded-full bg-secondary-100/90 dark:bg-gray-100/90 dark:text-secondary-100 p-2 text-gray-100" aria-label="Ver repositorio en GitHub">
+          <FaGithub />
+        </a>
+        <a href="https://codepen.io/rodyhuancas" target="_blank" className="rounded-full bg-secondary-100/90 dark:bg-gray-100/90 dark:text-secondary-100 p-2 text-gray-100" aria-label="Ver codepen en GitHub">
+          <FaCodepen />
+        </a>
+      </div>
     </div>
   )
 }
